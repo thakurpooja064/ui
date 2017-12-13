64746d406d98507cfd331bd9f7b2adc16a14678f
thakurpooja <thakurpooja064@gmail.com>
2017-12-13 23:25:11 +0530

    my first commit

---
+++ calc/js/calc.js
@@ -0,0 +1,23 @@
+$(function(){
+  var num1 = 0
+  var num2 = 0
+  var op  = "+"
+  
+  $(':button').on('click', function(){
+    var _pval = $(':text').val()
+    var _btn  = this.value
+    if( _btn == "C" || _btn == "AC" || _btn == "MC" ){
+      $(':text').val("")
+    }else{
+      $(':text').val(_pval + "" + this.value)
+    }
+    
+    if( _btn == "="){
+      var input = $(':text').val()
+      input = input.split("+")
+      num1 = parseInt( input[0])
+      num2 = parseInt( input[1])
+      $(':text').val( num1 + num2 )
+    }
+  });
+})
---
+++ calc/p.javascript
@@ -0,0 +1,6 @@
+
+function sum(a,b){
+ var c=a+b;
+document.write(c)
+}
+
